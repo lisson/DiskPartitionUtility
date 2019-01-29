19ec71b73c37eee84c1d9befa2d444dd1dc424d5
andy <hmm@hmm.com>
2019-01-29 00:55:28 -0800

    Add project files.

---
+++ DiskUtility/DiskUtility.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Management.Infrastructure;
+
+
+namespace DiskPartitionUtility
+{
+    public class Utility
+    {
+        private CimSession cimSession;
+
+        public Utility()
+        {
+            cimSession = CimSession.Create("localhost");
+        }
+
+        public DiskDrive[] GetDiskPartitions()
+        {
+            List<DiskDrive> disks = new List<DiskDrive>();
+            IEnumerable<CimInstance> win32_diskdrive = cimSession.QueryInstances(@"root\cimv2",
+                            "WQL",
+                            @"select * Size from win32_DiskDrive Where DeviceID");
+            string query;
+            foreach (CimInstance d in win32_diskdrive)
+            {
+                query = String.Format("ASSOCIATORS OF {{Win32_DiskDrive.DeviceID=\"{0}\"}} WHERE AssocClass=Win32_DiskDriveToDiskPartition",
+                    d.CimInstanceProperties["DeviceID"].ToString().Replace(@"\", @"\\"));
+                IEnumerable<CimInstance> partitionmap = cimSession.QueryInstances(@"root\cimv2",
+                            "WQL",
+                            query);
+            }
+
+            return null;
+        }
+
+    }
+
+    public class DiskDrive
+    {
+        private DiskPartition[] m_partitions;
+
+        public DiskDrive()
+        {
+
+        }
+
+        public DiskPartition[] Partitions
+        {
+            get { return m_partitions; }
+            set { m_partitions = value; }
+        }
+    }
+
+    public class DiskPartition
+    {
+        public DiskPartition()
+        {
+
+        }
+
+        private DiskVolume m_vol;
+
+        public DiskVolume Volume
+        {
+            get { return m_vol; }
+            set { m_vol = value; }
+        }
+
+
+        private int m_index;
+
+        public int Index
+        {
+            get { return m_index; }
+            set { m_index = value; }
+        }
+        private long m_size;
+
+        public long Size
+        {
+            get { return m_size; }
+            set { m_size = value; }
+        }
+    }
+
+    public class DiskVolume
+    {
+        private string m_letter;
+
+        public string Letter
+        {
+            get { return m_letter; }
+            set { m_letter = value; }
+        }
+
+        private int m_index;
+
+        public int Index
+        {
+            get { return m_index; }
+            set { m_index = value; }
+        }
+
+        private string m_label;
+
+        public string Label
+        {
+            get { return m_label; }
+            set { m_label = value; }
+        }
+
+        public DiskVolume()
+        {
+            
+        }
+    }
+}
---
+++ wmitest1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Management.Infrastructure;
+using System.Text.RegularExpressions;
+
+
+
+namespace wmitest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CimSession cimSession = CimSession.Create("localhost");
+            IEnumerable<CimInstance> queryInstances = cimSession.QueryInstances(@"root\cimv2",
+                            "WQL",
+                            @"select DeviceID, Model, Size from win32_DiskDrive Where DeviceID like '%PHYSICALDRIVE3'");
+
+            string disk="";
+
+            foreach (CimInstance cimInstance in queryInstances)
+            {
+             
+                Console.WriteLine("DeviceID name: {0}", cimInstance.CimInstanceProperties["DeviceID"].Value);
+                disk = cimInstance.CimInstanceProperties["DeviceID"].Value.ToString();
+                Console.WriteLine("Model name: {0}", cimInstance.CimInstanceProperties["Model"].Value);
+            }
+            
+            string query = String.Format("ASSOCIATORS OF {{Win32_DiskDrive.DeviceID=\"{0}\"}} WHERE AssocClass=Win32_DiskDriveToDiskPartition", disk.Replace(@"\", @"\\"));
+            Console.Out.WriteLine(query);
+            IEnumerable<CimInstance> partitionmap = cimSession.QueryInstances(@"root\cimv2",
+                            "WQL",
+                            query);
+            string query2;
+            IEnumerable<CimInstance> vols;
+            foreach (CimInstance p in partitionmap)
+            {
+                Console.WriteLine("DeviceID: {0}", p.CimInstanceProperties["DeviceID"].Value);
+                query2 = String.Format("ASSOCIATORS OF {{Win32_DiskPartition.DeviceID=\"{0}\"}} WHERE AssocClass=Win32_LogicalDiskToPartition", p.CimInstanceProperties["DeviceID"].Value);
+                vols = cimSession.QueryInstances(@"root\cimv2", "WQL", query2);
+                foreach (CimInstance v in vols)
+                {
+                    Console.WriteLine("Name: {0}", v.CimInstanceProperties["Name"].Value);
+                    Console.WriteLine("Size: {0}", v.CimInstanceProperties["Size"].Value);
+                    // "System Reserved" and "EFI" are windows partitions. Leave them alone
+                    Console.WriteLine("Size: {0}", v.CimInstanceProperties["VolumeName"].Value);
+                }
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
